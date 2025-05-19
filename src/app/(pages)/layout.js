621c44b3be3a377fe161fd4222dd43f5fa9c4ab0
Alish <alishallawadhi5@gmail.com>
2025-05-19 10:15:22 +0530

    Refactor layout.js to update API endpoint for fetching photographers and streamline data handling in useEffect.

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,34 +1,35 @@
 "use client";
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import useFetch from '../../customHooks/useFetch';
-import { setPhotographers, setLoading, setError } from '../../redux/slice/photographerSlice';
-import Header from '../../components/Header';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import useFetch from "../../customHooks/useFetch";
+import {
+  setPhotographers,
+  setLoading,
+  setError,
+} from "../../redux/slice/photographerSlice";
+import Header from "../../components/Header";
 
 export default function PagesLayout({ children }) {
   const dispatch = useDispatch();
-  const { data, loading, error } = useFetch('http://localhost:3002/photographers');
+  const { data, loading, error } = useFetch(
+    "https://moccasin-petra-75.tiiny.site/db.json"
+  );
 
   useEffect(() => {
     if (data) {
-      console.log('Fetched photographer data:', data);
-      console.log('First photographer in fetched data:', data[0]);
-      console.log('Photographer IDs in fetched data:', data.map(p => p.id));
-      dispatch(setPhotographers(data));
+      dispatch(setPhotographers(data.photographers));
     }
     dispatch(setLoading(loading));
     if (error) {
-      console.error('Error fetching photographers:', error);
+      console.error("Error fetching photographers:", error);
       dispatch(setError(error));
     }
   }, [data, loading, error, dispatch]);
-    
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 bg-gray-50 mt-14">
-        {children}
-      </main>
+      <main className="flex-1 bg-gray-50 mt-14">{children}</main>
     </div>
   );
 }
